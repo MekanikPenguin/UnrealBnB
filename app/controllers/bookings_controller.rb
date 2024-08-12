5621e2fa73567e69ce3890a1588c6f31f6b388fd
Sylvain Bertrand <sylv.bertrand@orange.fr>
2024-08-12 15:17:29 +0200

    Offers and Bookings Controllers

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  before_action :set_offer, only: [:destroy]
+
+  def index
+    @offers = Offer.all
+  end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(offer_params)
+
+    if @offer.save
+      redirect_to @offer
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @offer.destroy
+    redirect_to offers_url, status: :see_other
+  end
 end
