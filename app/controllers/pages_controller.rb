8f46031b7abd3d7dfb87c9c6406669e3956d5acc
Gabrielle Rochefort <gabrochefort@gmail.com>
2024-08-13 15:16:08 +0200

    remake of routes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,10 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def mybookings
+  end
+
+  def myoffers
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "offers#index"
-  get "/myoffers", to: "pages#myoffers"
-  get "/mybookings", to: "pages#mybookings"
+  get "/myoffers", to: "pages#myoffers", as: "myoffers"
+  get "/mybookings", to: "pages#mybookings", as: "mybookings"
   resources :offers do
     resources :bookings, only: [:create,:destroy]
   end
