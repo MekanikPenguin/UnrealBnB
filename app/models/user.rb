f4bbdc4f8fd45f9ced255b95b5a0714a0ddf2c9e
Gabrielle Rochefort <gabrochefort@gmail.com>
2024-08-14 16:59:05 +0200

    WTF

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,19 +11,9 @@ class User < ApplicationRecord
   has_many :offers
   has_many :bookings, dependent: :destroy
 
-  after_initialize :set_defaults
-
   #validations
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
   validates :first_name, presence: true
   validates :last_name, presence: true
-
-  private
-
-  def set_defaults
-    if self.url_image == nil
-      self.url_image = "https://kitt.lewagon.com/placeholder/users/ssaunier"
-    end
-  end
 end
