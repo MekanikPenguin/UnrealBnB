79e912e4ef24e08a46c3f1f62557a92ad9a2ff23
Sylvain Bertrand <sylv.bertrand@orange.fr>
2024-08-15 12:18:00 +0200

    improve bookings

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,13 @@ Rails.application.routes.draw do
   root to: "offers#index"
   get "/myoffers", to: "pages#myoffers", as: "myoffers"
   get "/mybookings", to: "pages#mybookings", as: "mybookings"
+
   resources :offers do
-    resources :bookings, only: [:new,:create,:destroy]
+    resources :bookings, only: [:new, :create, :destroy] do
+      member do
+        patch 'accept'
+        patch 'reject'
+      end
+    end
   end
 end
