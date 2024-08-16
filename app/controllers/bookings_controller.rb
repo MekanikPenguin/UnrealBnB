23b4e02f1c421b158cb67af52b6fba1f9787f81b
Paul REBOH <paul@reboh.net>
2024-08-16 04:52:16 +0200

    Review comments

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,13 +1,18 @@
 class BookingsController < ApplicationController
+
+
+  # REVIEW:
+  # Plutot qu'un before_action avec une variable d'instance
+  #preferer utiliser une method `booking` (voir dessous)
   before_action :set_booking, only: [:destroy, :accept, :reject]
 
   def create
     @user = current_user
-    @offer = Offer.find(params[:offer_id])
-    @booking = Booking.new(booking_params)
+    @offer = Offer.find(params[:offer_id]) # <---- SCOPING! current_user.offers.find()
+    @booking = Booking.new(booking_params) # <---- SCOPING! current_user.bookings.create()
     @booking.user = @user
     @booking.offer = @offer
-    @booking.status = "pending"
+    @booking.status = "pending" # use Symbol instead
     if @booking.save
       redirect_to mybookings_path, notice: "Votre demande de réservation a été envoyée."
     else
@@ -15,13 +20,13 @@ class BookingsController < ApplicationController
     end
   end
 
-  def new
+  # def new
+  # end
 
-  end
-  def destroy
-    @booking.destroy
-    redirect_to mybookings_path, status: :see_other
-  end
+  # def destroy
+  #   @booking.destroy
+  #   redirect_to mybookings_path, status: :see_other
+  # end
 
   def accept
     @booking.update(status: 'accepted')
@@ -39,7 +44,17 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:start_date, :end_date)
   end
 
+  # def booking
+  #   Current.user.bookings.find(params[:id])
+  #   # or current_user.bookings.find(params[:id])
+  # end
+
+  # REVIEW:
+  # C'est vraiment pas beau ces methodes de setter pour les vars.
+  # Ils vous apprenent ça comme ça au Wagon?
   def set_booking
+    # REVIEW
+    # toujours nester les methods accessors, voir method ci dessus
     @booking = Booking.find(params[:id])
   end
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -9,9 +9,13 @@ class Booking < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
   validates :status, inclusion: { in: %w[pending accepted rejected] }
+  # REVIEW : Validate max length
 
   # Scopes to filter bookings by status
   scope :pending, -> { where(status: 'pending') }
   scope :accepted, -> { where(status: 'accepted') }
   scope :rejected, -> { where(status: 'rejected') }
+
+  # REVIEW : Validate date :
+  # validates :end_date, comparison: { greater_than: :start_date }
 end
